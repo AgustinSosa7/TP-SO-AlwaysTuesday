f010df3a623d0bdc8e5433cd2a1eb46b88fb3b81
feliperuizdiaz <fruizdiaz@frba.utn.edu.ar>
2024-05-31 17:09:46 +0000

    io ge terminada

--- kernel/src/kernel_entradaSalida.c
+++ kernel/src/kernel_entradaSalida.c
@@ -29,9 +29,9 @@ t_interfaz* validar_peticion(t_peticion* peticion, t_pcb* pcb){
 
     t_interfaz* interfaz = existe_la_interfaz(nombre_io, pcb);
     if(esta_conectada_la_interfaz(interfaz, pcb)){
-    validar_interfaz_admite_instruccion(interfaz, instruccion); 
+        validar_interfaz_admite_instruccion(interfaz, instruccion); 
     }else{
-    //enviar_proceso_a_exit();
+        //matar al hilo en el que me encuentro?
     }
     return interfaz;
 }
@@ -44,7 +44,9 @@ t_interfaz* existe_la_interfaz(char* nombre_io, t_pcb* pcb){
     if(interfaz != NULL){ //Es correcta esta condicion?
         return interfaz;
     }else{
-            //enviar_proceso_a_exit();
+            log_error(kernel_logger,"No se ha encontrado a la interfaz: %s. Proceso enviado a EXIT.\n", interfaz->nombre);
+            cambiar_estado(pcb, EXIT);
+            //matar al hilo en el que me encuentro?
           }
 }         
 bool esta_conectada_la_interfaz(t_interfaz* interfaz, t_pcb* pcb){
@@ -57,8 +59,12 @@ bool esta_conectada_la_interfaz(t_interfaz* interfaz, t_pcb* pcb){
     eliminar_paquete(paquete);
 
     if(err == -1){
-        log_error(kernel_logger,"La interfaz %s no se encuentra conectada.", interfaz->nombre);
-        //enviar_proceso_a_exit();
+        pthread_mutex_lock(mutex_io);
+        list_remove_element(IOS_CONECTADOS, interfaz);
+        pthread_mutex_unlock(mutex_io);
+
+        log_error(kernel_logger,"La interfaz %s no se encuentra conectada. Proceso enviado a EXIT.\n", interfaz->nombre);
+        cambiar_estado(pcb, EXIT);
         return false;
     }else{
         bool respuesta = recibir_mensaje(fd_entradasalida);
@@ -78,7 +84,8 @@ void validar_interfaz_admite_instruccion(char* interfaz, char* instruccion, t_pc
 
       }else{
             log_error(kernel_logger,"Interfaz %s: No reconozco esta instruccion. Proceso enviado a EXIT.\n", interfaz);
-            //enviar_proceso_a_exit();
+            cambiar_estado(un_pcb, EXIT);
+            //matar al hilo en el que me encuentro?
       }
 }
 
@@ -93,43 +100,4 @@ void enviar_peticion_a_interfaz(t_peticion* peticion, t_interfaz* interfaz){
 void recibir_fin_peticion(){
     bool fin_peticion;
     recv(fd_entradasalida, &fin_peticion, sizeof(bool), MSG_WAITALL);
- 
 }
-
-void gestionar_lista_de_interfaz(t_peticion* peticion, t_interfaz* interfaz){
-
-    // si la interfaz no tiene procesos bloqueados, lo ejecuto
-    // si la interfaz tiene procesos bloqueados, lo agrego a la lista
-
-    // Hilo que maneja la cola de bloqueados:
-    // la cola de execute es un while(1) que se le avisa que ingreso un proceso a su cola mediante 
-    // un semaforo; este procede a hacerle pop, mandarlo a la cpu, esperar a recibir el contexto actualizado 
-    // y asi sucesivamente.
-
- //   while(1){
- //       wait(io->semaforo_cola_procesos_blocked);
- //       pcb = pop(io->cola_procesos_blocked);        
- //   }
-//
- //   // wait(se_libero_io); se inicializa en 1
- //   enviar_peticion_a_interfaz(peticion, interfaz);
-}
-
-
-
-struct{
-    char* nombre;
-    int fd_entradasalida;
-    t_queue cola_procesos_blocked;
-}t_interfaz
-
-struct{
-    char* nombre;
-    int fd_entradasalida;
-    t_queue cola_procesos_blocked;
-}bloqueados
-// int1 ----------------fd_entradasalida--------------- kernel -------------------cpu
-// 
-//                     p3 sleep 1500                           
-//                                                      
-//                                                      p4 sleep 4000
