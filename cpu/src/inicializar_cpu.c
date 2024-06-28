a2036a0f2e4740a7686e8a1fc610f052a3bc3426
AgustinSosa7 <agustinsosaherrera@gmail.com>
2024-06-28 20:26:48 +0000

    Solucionamos el problema con tamanio de pagina y de memoria

--- cpu/src/inicializar_cpu.c
+++ cpu/src/inicializar_cpu.c
@@ -79,8 +79,8 @@ void inicializar_estructuras(){
     string_array_push(&opcode_cpu, "IO_FS_READ");
 	string_array_push(&opcode_cpu, "EXIT");
 
-    tamanio_pagina = -1;
-    tamanio_memoria = -1;
+    //tamanio_pagina = -1;
+    //tamanio_memoria = -1;
 
     dejar_de_ejecutar = false;
     ocurrio_interrupcion = false;
