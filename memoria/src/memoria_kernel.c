7dcfc5db60aa310c5ce9c3deb742710ae66ffd3d
utnso <lucasluna068@gmail.com>
2024-07-11 05:53:16 +0000

    0 Memory leaks

--- memoria/src/memoria_kernel.c
+++ memoria/src/memoria_kernel.c
@@ -10,7 +10,7 @@ t_proceso* crear_proceso_nuevo(){
         //char* direccion_pseudocodigo = leer_string_del_buffer(buffer);
         procesoNuevo->pid = leer_int_del_buffer(buffer);
         procesoNuevo->direccion_pseudocodigo = leer_string_del_buffer(buffer);
-        procesoNuevo->instrucciones = list_create();
+        //procesoNuevo->instrucciones = list_create();
         procesoNuevo->instrucciones = leer_archivo_pseudocodigo(procesoNuevo->direccion_pseudocodigo);
         procesoNuevo->long_tabla_pags = 0;
         procesoNuevo->tabla_de_paginas = NULL;
@@ -25,6 +25,10 @@ t_proceso* crear_proceso_nuevo(){
         return procesoNuevo;
 };
 
+void eliminar_instruccion(char* instruccion){
+    free(instruccion);
+}
+
 void finalizar_proceso(){
     t_paquete* paquete = recibir_paquete(fd_kernel);
     t_buffer* buffer = paquete->buffer;
@@ -33,16 +37,18 @@ void finalizar_proceso(){
 
     t_proceso* proceso_eliminado = buscar_proceso_en_memoria(pid);
     liberar_marcos_memoria(proceso_eliminado,0); //el tamanio nuevo es 0 para que libere todos los marcos
-    
-    free(proceso_eliminado->direccion_pseudocodigo);//valgrind
-    list_destroy(proceso_eliminado->instrucciones);//valgrind (Ver como funciona el destroy elements)
 
     if(list_remove_element(procesos_memoria,proceso_eliminado)){
         //log_info(memoria_log_debug, "Finalizo el proceso: %d",proceso_eliminado->pid);
         
         //Log obligatorio. (Creación / destrucción de Tabla de Páginas:)
         log_info(memoria_logger, "PID: %d - Tamaño: %d ",proceso_eliminado->pid,proceso_eliminado->long_tabla_pags);// Evaluar si setear la long a 0 o mostrar las que tenia el proceso antes.
-                                                                                                                        //OPCION A Y CREO CORRECTA: SETEAR A 0 LA LONG DE TABLA DE PAGINAS. POR Q YA LIBERE LOS MARCOS.
+        
+        free(proceso_eliminado->direccion_pseudocodigo);//valgrind
+        free(proceso_eliminado->long_tabla_pags);
+        free(proceso_eliminado->tabla_de_paginas);//valgrind
+        list_destroy_and_destroy_elements(proceso_eliminado->instrucciones,(void*)eliminar_instruccion);//valgrind (Ver como funciona el destroy elements)
+        
         free(proceso_eliminado);
     }
     else
