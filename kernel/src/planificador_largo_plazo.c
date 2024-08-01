9a3fcc181d8c9271cb3c7ed16eefa29f7118d5e2
MilagrosCuevas <kcuevasvazquez@frba.utn.edu.ar>
2024-08-01 16:56:18 +0000

    comento logs innecesarios

--- kernel/src/planificador_largo_plazo.c
+++ kernel/src/planificador_largo_plazo.c
@@ -1,21 +1,19 @@
 #include "../includes/planificador_largo_plazo.h"
 
-
-
 void planif_largo_plazo(){
    while(1){
-      printf("Entro al plani largo plazo.\n");
+      //printf("Entro al plani largo plazo.\n");
 
-      printf("Pase funcion detener plani.\n");
-      printf("WAIT del plani LARGO plazo.\n");
+      //printf("Pase funcion detener plani.\n");
+      //printf("WAIT del plani LARGO plazo.\n");
       sem_wait(&sem_new_a_ready);
       detener_plani_largo_plazo();
  	   sem_wait(&sem_grado_multiprogram);
-      printf("Pase los semaforos del plani LARGO plazo.\n");
+      //printf("Pase los semaforos del plani LARGO plazo.\n");
       if(!list_is_empty(struct_new->lista)){
       cambiar_de_estado_y_de_lista(NEW, READY);
-      printf("Cambio de lista.\n");
-      printf("SIGNAL del plani CORTO plazo.\n");
+      //printf("Cambio de lista.\n");
+      //printf("SIGNAL del plani CORTO plazo.\n");
       sem_post(&sem_planificador_corto_plazo);
          }
       }
